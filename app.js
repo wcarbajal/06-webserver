be213f81b6194631d92c100018cb315e63ed595d
wuilmer carbajal <wcarbajal@outlook.com>
2025-07-03 20:53:59 -0500

    en la nuve de namecheap

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use( express.static('public'))
 app.get('/', (req, res) => {
   res.render('home', {
     nombre: 'Wuilmer Carbajal',
-    titulo: 'Curso de Node'
+    titulo: 'Portal de procesos'
   })
 })
 
